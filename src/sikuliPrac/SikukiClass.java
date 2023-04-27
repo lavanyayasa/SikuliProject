5bb178bcd028866cc4841ecb3c1827280f2f1552
nit104 <nit104@nareshit104.bbrouter>
2023-04-27 13:06:22 -0400

    uploading

---
+++ src/sikuliPrac/SikukiClass.java
@@ -0,0 +1,17 @@
+package sikuliPrac;
+
+
+
+import org.sikuli.script.Screen;
+
+public class SikukiClass {
+
+	public static void main(String[] args)  throws Exception{
+		
+		System.out.println("hfeherlo");
+		Screen obj=new Screen();
+		obj.click("D:\\snippingtool\\editplusmini.PNG");
+
+	}
+
+}
